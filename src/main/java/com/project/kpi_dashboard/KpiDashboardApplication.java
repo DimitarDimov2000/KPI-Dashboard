afc34122e9ec92c1cc0498625d68158b26659d14
unknown <dimitar.dimov2000@gmail.com>
2025-10-19 19:16:32 +0200

    Gradle and Spring Boot

---
+++ src/main/java/com/project/kpi_dashboard/KpiDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.project.kpi_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpiDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/kpi_dashboard/KpiDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kpi_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
